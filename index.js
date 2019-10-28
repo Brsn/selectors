af21dcc71c7459d52f454fa83851865f7520d47e
Bronson Brown <Brsn90@icloud.com>
2019-10-27 20:31:35 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hi");
+//function promptName() {
+    //var name = prompt("Please enter your name: ");
+    //var changeName = document.getElementById("three").innerHTML = name;
+    //////console.log(changeName);
+//}
+function changeName() {
+    var changeName = document.getElementById("change").innerHTML;
+    console.log(changeName);
+    changeName = nme;
+    console.log(changeName);
+}
+
+
+function submitForm() {
+    var textBox = document.getElementById("change").value;
+    var wtf = document.getElementById("three").innerHTML = textBox;
+   
+}
